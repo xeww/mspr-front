500a34c091c579259b731be916eaa7e60d5a6904
François L.P. <me@francois-lp.dev>
2024-11-01 08:08:43 +0100

    feat: header work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import "./Header.css";
 import MenuIcon from "../Icons/MenuIcon.jsx";
+import { useEffect, useRef } from "react";
 
 export default function Header() {
+  const headerRef = useRef(null);
+  const transparent = "rgba(217, 217, 217, 0.03)";
+  const opaque = "var(--dark)";
+
+  const handleScroll = () => {
+    if (document.documentElement.scrollTop > 50) {
+      headerRef.current.style.backgroundColor = opaque;
+      headerRef.current.style.borderBottom = "1px solid white";
+    } else {
+      headerRef.current.style.backgroundColor = transparent;
+      headerRef.current.style.borderBottom = "none";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header>
+      <header ref={headerRef}>
         <h1 className="font-title">NATION SOUNDS</h1>
         <MenuIcon />
       </header>
