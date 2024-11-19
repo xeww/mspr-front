e21e2f07fb708744ba70833ca2678dc30b3c588b
François L.P. <me@francois-lp.dev>
2024-11-19 05:00:21 +0100

    feat: program page filters (in-progress)

---
+++ src/pages/program/filters/filters.jsx
@@ -0,0 +1,99 @@
+import "./filters.css";
+import UserStarIcon from "../../../components/icons/user-star-icon.jsx";
+import ExpandIcon from "../../../components/icons/expand-icon.jsx";
+import { useContext, useEffect, useRef, useState } from "react";
+import useArtist from "../../../hooks/useArtist.js";
+import useScene from "../../../hooks/useScene.js";
+import SceneIcon from "../../../components/icons/scene-icon.jsx";
+import { FilterContext } from "../program-page.jsx";
+
+export default function Filters() {
+  const artists = useArtist();
+  const scenes = useScene();
+
+  return (
+    <section className="filters-container">
+      <SingleFilter
+        defaultStr="Artiste"
+        menu={<FilterListMenu choices={artists} type="artist" />}
+        leftIcon={<UserStarIcon />}
+        rightIcon={<ExpandIcon />}
+      />
+      <SingleFilter
+        defaultStr="Scène"
+        menu={<FilterListMenu choices={scenes} type="scene" />}
+        leftIcon={<SceneIcon />}
+        rightIcon={<ExpandIcon />}
+      />
+    </section>
+  );
+}
+
+function SingleFilter({ defaultStr, menu, leftIcon, rightIcon }) {
+  const [toggled, setToggled] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleClick = () => {
+    setToggled(!toggled);
+  };
+
+  useEffect(() => {
+    if (menuRef.current) {
+      menuRef.current.style.display = toggled ? "block" : "none";
+    }
+  }, [toggled]);
+
+  return (
+    <div>
+      <div className="single-filter" onClick={handleClick}>
+        {leftIcon}
+        <p className="font-title">{defaultStr}</p>
+        <div className="filter-expand-icon">{rightIcon}</div>
+      </div>
+      <div ref={menuRef}>{menu}</div>
+    </div>
+  );
+}
+
+function FilterListMenu({ choices, type }) {
+  const { filterSelection, setFilterSelection } = useContext(FilterContext);
+
+  const handleClick = (item) => {
+    if (filterSelection[type].includes(item)) {
+      /* Removing from the filter selection*/
+      const updatedSelection = filterSelection[type].filter(
+        (selectedItem) => selectedItem !== item,
+      );
+      setFilterSelection({ ...filterSelection, [type]: updatedSelection });
+    } else {
+      /* Adding to the filter selection */
+      const updatedSelection = [...filterSelection[type], item];
+      setFilterSelection({ ...filterSelection, [type]: updatedSelection });
+    }
+  };
+
+  useEffect(() => {
+    if (choices) {
+      setFilterSelection({ ...filterSelection, [type]: choices });
+    }
+  }, [choices]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className="filter-menu">
+      {choices &&
+        choices.map((item) => (
+          <p
+            key={item.id}
+            className={`font-body filter-menu-choice ${
+              filterSelection[type].includes(item)
+                ? " filter-menu-choice-selected"
+                : ""
+            }`}
+            onClick={() => handleClick(item)}
+          >
+            {item.name}
+          </p>
+        ))}
+    </div>
+  );
+}
--- src/pages/program/program-page.jsx
+++ src/pages/program/program-page.jsx
@@ -1,71 +1,102 @@
-/* Stylesheets imports */
 import "./program-page.css";
-
-/* Components imports */
 import Header from "../../components/header/header.jsx";
 import Footer from "../../components/footer/footer.jsx";
 import UpperPage from "../../components/upper-page/upper-page.jsx";
 import MarginWrapper from "../../components/margin-wrapper/margin-wrapper.jsx";
 import InfoBubble from "../../components/info-bubble/info-bubble.jsx";
 import ConcertCard from "./concert-card/concert-card.jsx";
-
-/* Lib imports */
 import useConcert from "../../hooks/useConcert.js";
-import { useRef } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useLocation } from "wouter";
 import { getFullDate, getHour } from "../../utils/date.js";
+import Filters from "./filters/filters.jsx";
+
+export const FilterContext = createContext(null);
+
+function FilterProvider({ children }) {
+  const [filterSelection, setFilterSelection] = useState({
+    artist: [],
+    scene: [],
+    date: [],
+  });
+
+  return (
+    <FilterContext.Provider value={{ filterSelection, setFilterSelection }}>
+      {children}
+    </FilterContext.Provider>
+  );
+}
 
 export default function ProgramPage() {
   return (
-    <>
+    <FilterProvider>
       <Header />
       <UpperPage
         title="Le Programme"
         description="Découvrez le programme que nous avons préparer!"
       />
       <MarginWrapper>
+        <InfoBubble
+          title="Filtres"
+          description="Trouvez le concert qui vous convient."
+        />
         <Filters />
         <Concerts />
       </MarginWrapper>
       <Footer />
-    </>
-  );
-}
-
-function Filters() {
-  return (
-    <section className="filters-container">
-      <InfoBubble
-        title="Filtres"
-        description="Trouvez le concert qui vous convient."
-      />
-    </section>
+    </FilterProvider>
   );
 }
 
 function Concerts() {
-  const container = useRef(null);
+  const container = useRef({});
   const concerts = useConcert();
   const [, setLocation] = useLocation();
+  const { filterSelection } = useContext(FilterContext);
+  const [filteredConcerts, setFilteredConcerts] = useState([]);
 
   const handleClick = (id) => {
     setLocation(`/concert/${id}`);
   };
 
+  const handleFilters = () => {
+    const sceneSelectionIds = filterSelection["scene"].map((scene) => scene.id);
+    const artistSelectionIds = filterSelection["artist"].map(
+      (artist) => artist.id,
+    );
+
+    if (concerts) {
+      let filtered = concerts.filter((concert) =>
+        sceneSelectionIds.includes(concert.scene.id),
+      );
+
+      filtered = filtered.filter((concert) =>
+        artistSelectionIds.includes(concert.artist.id),
+      );
+
+      setFilteredConcerts(filtered);
+    } else {
+      setFilteredConcerts([]);
+    }
+  };
+
+  useEffect(() => {
+    handleFilters();
+  }, [filterSelection]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <section className="concerts-container" ref={container}>
-      {concerts &&
-        concerts.map((concert) => (
-          <ConcertCard
-            key={concert.id}
-            image={`${import.meta.env.VITE_IMAGES_URL}/${concert.artist.imageName}`}
-            artistName={concert.artist.name}
-            date={getFullDate(concert.dateAndTime)}
-            time={getHour(concert.dateAndTime)}
-            sceneName={concert.scene.name}
-            onClick={() => handleClick(concert.id)}
-          />
-        ))}
+      {filteredConcerts.map((concert) => (
+        <ConcertCard
+          key={concert.id}
+          image={`${import.meta.env.VITE_IMAGES_URL}/${concert.artist.imageName}`}
+          artistName={concert.artist.name}
+          date={getFullDate(concert.dateAndTime)}
+          time={getHour(concert.dateAndTime)}
+          sceneName={concert.scene.name}
+          onClick={() => handleClick(concert.id)}
+        />
+      ))}
     </section>
   );
 }
