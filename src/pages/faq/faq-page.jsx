fb7f1e40e7da0caf09aa1ff2964bfcccd99a8f55
François L.P. <me@francois-lp.dev>
2024-12-02 18:19:43 +0100

    feat: faq

--- src/pages/faq/faq-page.jsx
+++ src/pages/faq/faq-page.jsx
@@ -14,6 +14,10 @@ export default function FaqPage() {
       <Header />
       <UpperPage title="FAQ & Contact" description="Des questions?" />
       <MarginWrapper>
+        <InfoBubble
+          title="Questions fréquentes"
+          description="Quelques questions souvent demandées"
+        />
         <Questions />
         <InfoBubble
           title="D&#39;autres questions?"
@@ -30,38 +34,48 @@ export function Questions() {
   return (
     <section className="faq-questions-container">
       <Question
-        question="Lorem ipsum dolor sit amet, consectetur adipiscing elit?"
-        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et
-         dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-         commodo consequat."
+        question="Quelles sont les dates du festival ?"
+        answer="Le festival se déroulera à partir de début juin jusqu'au mois de juillet. Assurez-vous de noter ces dates dans votre agenda pour ne pas manquer les événements !"
       />
 
       <Question
-        question="Lorem ipsum dolor sit amet, consectetur adipiscing elit?"
-        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et
-         dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-         commodo consequat."
+        question="Où se déroule le festival ?"
+        answer="Le festival se tient au bois de vincennes, dans Paris. Vous trouverez un plan détaillé du site sur notre page 'Carte' pour faciliter votre venue."
       />
 
       <Question
-        question="Lorem ipsum dolor sit amet, consectetur adipiscing elit?"
-        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et
-         dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-         commodo consequat."
+        question="Comment puis-je acheter des billets ?"
+        answer="Vous pouvez acheter vos billets directement sur le site de notre partenaire, Ticketmaster, veuillez vous cliquer sur la catégorie 'Billetterie' dans le menu pour plus d'information."
       />
 
       <Question
-        question="Lorem ipsum dolor sit amet, consectetur adipiscing elit?"
-        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et
-         dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-         commodo consequat."
+        question="Y a-t-il des réductions pour les étudiants ou les groupes ?"
+        answer="Oui, nous offrons des réductions spéciales pour les étudiants, ainsi que des tarifs de groupe. Pour plus de détails, consultez la page 'Billetterie' ou contactez notre équipe à l'aide du formulaire de contact."
       />
 
       <Question
-        question="Lorem ipsum dolor sit amet, consectetur adipiscing elit?"
-        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et
-         dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-         commodo consequat."
+        question="Puis-je ramener ma nourriture ou mes boissons ?"
+        answer="Pour des raisons de sécurité et de réglementation, il n’est pas permis de ramener de nourriture ou de boissons de l’extérieur. Toutefois, de nombreux stands de nourriture et de boissons seront disponibles sur place."
+      />
+
+      <Question
+        question="Puis-je venir avec mon chien ou mon animal de compagnie ?"
+        answer="Les animaux de compagnie ne sont pas autorisés sur le site du festival, sauf pour les animaux d'assistance. Nous vous prions de respecter cette règle pour le confort de tous."
+      />
+
+      <Question
+        question="Quelles mesures sont prises en matière de sécurité ?"
+        answer="La sécurité de nos festivaliers est notre priorité. Des contrôles de sécurité seront effectués à l’entrée du site. Merci de venir sans objets interdits (armes, bouteilles en verre, etc.) et d’être prêt à coopérer avec nos agents de sécurité."
+      />
+
+      <Question
+        question="Est-ce que le festival est accessible aux personnes en situation de handicap ?"
+        answer="Oui, le festival est accessible aux personnes en situation de handicap. Des rampes, des places de parking dédiées et des sanitaires adaptés sont disponibles sur le site. Si vous avez des besoins spécifiques, n’hésitez pas à nous contacter avant votre venue."
+      />
+
+      <Question
+        question="Quelles sont les règles concernant les photographies et vidéos ?"
+        answer="La prise de photos est autorisée pour un usage personnel. Cependant, l'utilisation de caméras professionnelles ou de drones est interdite sans autorisation préalable. Des photographes et vidéastes officiels seront présents sur place, et nous vous invitons à respecter leur travail."
       />
     </section>
   );
